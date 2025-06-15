d5d402443157dd2dce5d2ea308e2fc766d07cf75
Maria Cecília <cecicaporale16@gmail.com>
2025-06-15 18:16:31 -0300

    feat: adicionado theme

---
+++ shadow-lab/src/styles/theme.ts
@@ -0,0 +1,21 @@
+export const theme = {
+  colors: {
+    background: '#121212',
+    backgroundPreview: '#DEDCDD',
+    surface: '#4f4f4f',
+    muted: '#ACACAA',
+    pink: '#D13880',
+    blue: '#0AB7C1',
+    yellow: '#FBD318',
+    black: '#000000',
+    white: '#FFFFFF',
+    text: '#000',
+  },
+  spacing: {
+    xs: '4px',
+    sm: '8px',
+    md: '16px',
+    lg: '32px',
+    xl: '64px',
+  },
+};
