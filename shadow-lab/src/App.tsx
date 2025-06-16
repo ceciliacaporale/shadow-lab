84e84fa3154cdfa0cf2b84fcd57b968f24dc6e9b
Maria Cecília <cecicaporale16@gmail.com>
2025-06-16 00:02:14 -0300

    feat: importando componentes no App

--- shadow-lab/src/App.tsx
+++ shadow-lab/src/App.tsx
@@ -1,11 +1,20 @@
+import Header from "./components/Header";
+import { ThemeProvider } from 'styled-components';
+import { theme } from "./styles/theme";
 import { GlobalStyle } from "./styles/GlobalStyles";
+import MainContainer from "./components/layout/MainContainer";
+import Footer from "./components/Footer/";
 
 function App() {
 
   return (
     <>
+    <ThemeProvider theme={theme}>
     <GlobalStyle />
-      
+    <Header />
+    <MainContainer />
+    <Footer />
+    </ThemeProvider>
     </>
   )
 }
